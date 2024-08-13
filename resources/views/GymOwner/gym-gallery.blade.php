114b8638cdd6170673db7ab7b150f2812e3cdc40
Muskan Shrivastav <mwingersitservices@gmail.com>
2024-08-13 17:58:52 +0530

    gym gallery design

--- resources/views/GymOwner/gym-gallery.blade.php
+++ resources/views/GymOwner/gym-gallery.blade.php
@@ -10,11 +10,40 @@
 </ol>
 </div>
 <div class="row">
+    <div class="modal fade" id="addimage">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Add New Plan</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal">
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form method="" action="">
+                        @csrf
+                        <div class="form-group">
+                            <label>Upload Image/Video</label>
+                            <input type="file" class="form-control" id="image" name="image" required="">
+                                            <div class="invalid-feedback">
+                                                Gym Image is required.
+                                            </div>
+                        </div>
+
+
+                        <button class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
 <div class="col-lg-12">
     <div class="card">
         <div class="card-header">
             <h4 class="card-title">Gallery</h4>
+            <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#addimage" class="btn btn-outline-primary rounded">Add New Image</a>
         </div>
+
         <div class="card-body pb-1">
             <div id="lightgallery" class="row">
                 <a href="https://fito.dexignzone.com/laravel/demo/images/big/img1.jpg" data-exthumbimage="https://fito.dexignzone.com/laravel/demo/images/big/img1.jpg" data-src="https://fito.dexignzone.com/laravel/demo/images/big/img1.jpg" class="col-lg-3 col-md-6 mb-4">
