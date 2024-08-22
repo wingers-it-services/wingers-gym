b4544a0ae30f3b96bdd2f44ef3bdc39665a780d7
Anjali Tiwari <anjalitiwari67269@gmail.com>
2024-08-22 17:46:31 +0530

    all tables models related to shop is set

--- app/Models/ClothImage.php
+++ app/Models/ClothImage.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid;
 
 class ClothImage extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'from',
+        'to',
+        'image',
+        'description',
+        'users',
+        'status'
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->uuid = Uuid::uuid4()->toString();
+        });
+    }
 }
