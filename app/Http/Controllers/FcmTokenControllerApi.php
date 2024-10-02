cabc2be1b9352e8cfe8634bce3c60aa2812899d0
Vishal Thakor <vishal.wingersitservices@gmail.com>
2024-10-02 18:57:23 +0530

    added code to send fcm notification.

--- app/Http/Controllers/FcmTokenControllerApi.php
+++ app/Http/Controllers/FcmTokenControllerApi.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\UserFcmToken;
+use App\Services\NotificationService;
 use App\Traits\errorResponseTrait;
 use Exception;
 use Illuminate\Http\Request;
@@ -14,11 +15,14 @@ class FcmTokenControllerApi extends Controller
 
     use errorResponseTrait;
     private $userFcmToken;
+    private $notificationService;
 
     public function __construct(
-        UserFcmToken $userFcmToken
+        UserFcmToken $userFcmToken,
+        NotificationService $notificationService
     ) {
         $this->userFcmToken = $userFcmToken;
+        $this->notificationService = $notificationService;
     }
 
     public function addUserFcmToken(Request $request)
@@ -46,5 +50,13 @@ class FcmTokenControllerApi extends Controller
             Log::error('[FcmTokenControllerApi][addUserFcmToken] Error adding token: ' . $e->getMessage());
             return $this->errorResponse('Error while adding fcm token', $e->getMessage(), 500);
         }
+    }
+
+    public function sendNotification()
+    {
+        $tokens = ['fy6Gp5bkT-e39UVJtBnQXR:APA91bHc_Q1oGVh_oVz3ST2x3VbKJr-5D-jhFrDk42uIefGlQsy_DH46hgevAWhi2pr9-wrLHU8F7zpj7dQDlyyGFQ7bahpvIFSezSJOn-Z_BGKLF_1Mrmwb613DN_m2_ND7DwoJOAvz'];
+        $title = "Breaking News";
+        $message = "Here is the Notification from server.";
+        $this->notificationService->sendNotification($tokens, $title, $message, null);
     }
 }
