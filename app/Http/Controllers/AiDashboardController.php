6ee9ca3a6117a4434ccf25f44b1c00f05bba1c8f
Anjali Tiwari <anjalitiwari67269@gmail.com>
2024-10-18 11:55:37 +0530

    fetch user location

--- app/Http/Controllers/AiDashboardController.php
+++ app/Http/Controllers/AiDashboardController.php
@@ -9,16 +9,32 @@ use Illuminate\Support\Facades\Log;
 
 class AiDashboardController extends Controller
 {
+    protected $userLatitudeAndLongitude;
+
+    public function __construct(
+        UserLatitudeAndLongitude $userLatitudeAndLongitude
+    ) {
+        $this->userLatitudeAndLongitude = $userLatitudeAndLongitude;
+    }
+
     public function getUserLocations()
     {
-        // Fetch users with their names and locations
-        $locations = UserLatitudeAndLongitude::select('gym_users.firstname', 'user_latitude_and_longitudes.latitude', 'user_latitude_and_longitudes.longitude')
-            ->join('gym_users', 'user_latitude_and_longitudes.user_id', '=', 'gym_users.id') // Join with users table
-            ->whereNotNull('user_latitude_and_longitudes.latitude')
-            ->whereNotNull('user_latitude_and_longitudes.longitude')
-            ->get();
-
-            Log::info($locations);
+        $gym = Auth::guard('gym')->user();
+        if ($gym->gym_type == 'admin') {
+            $locations = $this->userLatitudeAndLongitude->select('gym_users.firstname', 'user_latitude_and_longitudes.latitude', 'user_latitude_and_longitudes.longitude')
+                ->join('gym_users', 'user_latitude_and_longitudes.user_id', '=', 'gym_users.id')
+                ->whereNotNull('user_latitude_and_longitudes.latitude')
+                ->whereNotNull('user_latitude_and_longitudes.longitude')
+                ->get();
+        } else {
+            $locations = $this->userLatitudeAndLongitude->select('gym_users.firstname', 'user_latitude_and_longitudes.latitude', 'user_latitude_and_longitudes.longitude')
+                ->join('gym_users', 'user_latitude_and_longitudes.user_id', '=', 'gym_users.id')
+                ->join('gym_user_gyms', 'gym_users.id', '=', 'gym_user_gyms.user_id')
+                ->where('gym_user_gyms.gym_id', $gym->id)
+                ->whereNotNull('user_latitude_and_longitudes.latitude')
+                ->whereNotNull('user_latitude_and_longitudes.longitude')
+                ->get();
+        }
 
         return response()->json($locations);
     }
